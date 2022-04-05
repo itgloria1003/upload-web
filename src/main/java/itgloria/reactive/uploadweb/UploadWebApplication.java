033c6e348023792dc59727ae0cb4698fc71a02c1
itgloria1003 <itgloria1003@gmail.com>
2022-04-05 14:28:15 +0800

    initial version

---
+++ src/main/java/itgloria/reactive/uploadweb/UploadWebApplication.java
@@ -0,0 +1,13 @@
+package itgloria.reactive.uploadweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadWebApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itgloria/reactive/uploadweb/UploadWebApplicationTests.java
@@ -0,0 +1,13 @@
+package itgloria.reactive.uploadweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
